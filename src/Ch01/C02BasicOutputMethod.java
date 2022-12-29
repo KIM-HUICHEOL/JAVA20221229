8df15ced9666b778243fbc8b802a9d348a8e728f
KIM-HUICHEOL <kimhc0304@naver.com>
2022-12-29 18:01:15 +0900

    sesres

---
+++ src/Ch01/C02BasicOutputMethod.java
@@ -0,0 +1,23 @@
+package Ch01;
+
+public class C02BasicOutputMethod {
+
+	public static void main(String[] args) {
+		
+		// System : OS관련 자원들을 저장하는 단위
+		// .	  : --안에
+		// out    : 표준출력장치
+		
+		// System.out.print()
+		// 기본 출력함수
+		// Escape 문자
+		System.out.print("HELLOWORLD\n");
+		System.out.print("HELLOWORLD\n");
+		
+		// System.out.printf()
+		
+		// System.out.println()
+
+	}
+
+}
